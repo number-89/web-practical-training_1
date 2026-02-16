20d00c4d501a2e57fb3ae94fe250a8a3ca57e1bb
Nabara <M2520038@ngo.ohara.ac.jp>
2026-02-16 15:06:44 +0900

    ハンバーガーメニューをクリックしたとき、画面が暗転するようにしたいが、ハンバーガーメニューが奥に行ってしまい、一度しか押せなくなっている。

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,20 @@
-let isMenuOpen = false;
+// ハンバーガーメニュー
 const menu = document.querySelector(".menu");
+const menuSpan = document.querySelectorAll(".menu span");
+const blackout = document.querySelector("#blackout");
+
 menu.addEventListener("click", () => {
-    if(isMenuOpen){
+    if(menu.id == "open-menu"){
         menu.id = "close-menu";
         isMenuOpen = !isMenuOpen;
+        blackout.style.visibility = "hidden";
     } else {
         menu.id ="open-menu";
+        blackout.style.visibility = "visible";
         isMenuOpen = !isMenuOpen;
+
+        menuSpan.forEach(e => {
+            // e.style.backgroundColor = "white";
+        })
     }
 })
