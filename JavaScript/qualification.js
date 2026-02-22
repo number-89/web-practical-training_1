94993d16915292608a0757ce6bbe60bf5f6b0406
Nabara <M2520038@ngo.ohara.ac.jp>
2026-02-23 05:02:51 +0900

    サイドウィンドウの開閉アニメーションを一部作成

--- JavaScript/qualification.js
+++ JavaScript/qualification.js
@@ -20,3 +20,56 @@ menu.addEventListener("click", () => {
         });
     }
 });
+
+// ----------説明文----------
+const sideWindow = document.querySelector("#qualification-story")  // サイドメニュー
+const closeBtn = document.querySelector("#close-qualification-story"); // ボタン
+const qualificationNameList = document.querySelectorAll(".qualification-name"); // 取得した資格のリスト
+const storyTextList = document.querySelectorAll("#qualification-story-text li p"); // 説明文のリスト
+let sideWindowWidth = sideWindow.clientWidth; // サイドメニューの横幅(px)
+
+let isWindowOpen = true;
+closeBtn.onclick = sideWindowClose;
+
+// ウィンドウを閉じる
+function sideWindowClose(){
+    if(isWindowOpen){
+        sideWindow.animate(
+            {
+                right:[0, `${-1 * (sideWindowWidth + 1)}px`]
+            },
+            {
+                duration:250,
+                fill:"forwards",
+                easing:"ease-out"
+            }
+        );
+
+        isWindowOpen = false
+    }
+}
+
+// ウィンドウを開くイベントを付与
+qualificationNameList.forEach((e, i) => {
+    e.onclick = sideWindowOpen;
+})
+
+function sideWindowOpen(){
+    // 閉じているとき
+    if(!isWindowOpen){
+        sideWindow.animate(
+            {
+                right:[`${-1 * (sideWindowWidth + 1)}px`, 0]
+            },
+            {
+                duration:250,
+                fill:"forwards",
+                easing:"ease-out"
+            }
+        );
+
+        isWindowOpen= true;
+    } else {
+        // 開いているとき
+    }
+}
