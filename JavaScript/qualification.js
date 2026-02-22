cf103dbffd808ea3e0d1b094d4b6c2e69b56d32f
Nabara <M2520038@ngo.ohara.ac.jp>
2026-02-23 04:03:49 +0900

    qualification.jsにハンバーガーメニューを追加

---
+++ JavaScript/qualification.js
@@ -0,0 +1,22 @@
+// ----------ハンバーガーメニュー----------
+const menu = document.querySelector(".menu");
+const menuSpan = document.querySelectorAll(".menu span");
+const blackout = document.querySelector("#blackout");
+
+menu.addEventListener("click", () => {
+    if(menu.id == "open-menu"){
+        menu.id = "close-menu";
+        blackout.style.visibility = "hidden";
+
+        menuSpan.forEach(e => {
+            e.style.backgroundColor = "black";
+        });
+    } else {
+        menu.id ="open-menu";
+        blackout.style.visibility = "visible";
+
+        menuSpan.forEach(e => {
+            e.style.backgroundColor = "white";
+        });
+    }
+});
